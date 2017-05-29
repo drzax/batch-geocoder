3ffaaa44ca8edbf3c5ac1b1f4d9cccb3ec36c628
Simon Elvery <simon@elvery.net>
2017-05-29 14:12:44 +1000

    Some changes I forget the meaning of—committed just in case.

--- app.js
+++ app.js
@@ -15,21 +15,27 @@ function initialize() {
 
 		var q = async.queue(function (task, callback) {
 			console.log('geocoding',task.addr);
-			codeAddress(task.addr,function(result){
+			codeAddress(task.addr,function(result, address){
 				if(result){
-					text[task.i] = text[task.i] + '\t' + result.lat() + '\t'+result.lng();
+					text[task.i] = address + '\t' + result.lat() + '\t'+result.lng();
 				}
 				document.querySelector('textarea').value = text.join("\n");
 				callback();
 			},true);
 		}, 2);
 
-		text.forEach(function(addr,i){
-			q.push({i: i,addr:addr}, function (err) {
-			    if(q.length() == 0){
-			    	console.log('Completed all')
-			    }
-			});
+		text.forEach(function(row,i){
+			var cols = row.split('\t').filter(function(d){return !!d;});
+			if (!cols[1] || !cols[2]) {
+				q.push({i:i, addr:cols[0]}, function (err) {
+					if(q.length() == 0){
+						console.log('Completed all')
+					}
+				});
+			} else {
+				text[i] = cols.join('\t');
+				document.querySelector('textarea').value = text.join("\n");
+			}
 		});
 	}
 }
@@ -43,7 +49,7 @@ function codeAddress(address,callback,retry) {
 					position: results[0].geometry.location
 			});
 			window.setTimeout(function(){
-				callback(results[0].geometry.location);
+				callback(results[0].geometry.location, address);
 			},Math.random()*2000);
 		} else {
 			console.log("Geocode was not successful for the following reason: " + status);
